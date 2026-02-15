dcd4f9a6852912875167d6f190c6efc11b57f7e7
Eric Langlois <langlois88@gmail.com>
2026-02-15 14:53:44 -0500

    Set STATIC_ROOT for collectstatic

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'portfolio.pagination.DefaultPagination',
