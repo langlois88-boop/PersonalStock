6228384f83bee0b267989870e6a338461563b2c3
Eric Langlois <langlois88@gmail.com>
2026-02-15 20:31:11 -0500

    Use 8001 for API base URL

--- portfolio-frontend/src/api/api.js
+++ portfolio-frontend/src/api/api.js
@@ -3,7 +3,7 @@ import { pushApiError } from './errorStore';
 
 const apiBaseUrl =
 	process.env.REACT_APP_API_BASE_URL ||
-	`${window.location.protocol}//${window.location.hostname}:8000/api/`;
+	`${window.location.protocol}//${window.location.hostname}:8001/api/`;
 
 const api = axios.create({ baseURL: apiBaseUrl, timeout: 10000 });
 
