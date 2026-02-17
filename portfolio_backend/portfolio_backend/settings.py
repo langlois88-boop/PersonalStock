e706521259c9147d00125da33610d17de5488e60
Eric Langlois <langlois88@gmail.com>
2026-02-16 19:51:50 -0500

    Add SMTP email settings

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -311,6 +311,12 @@ EMAIL_BACKEND = os.getenv(
     'EMAIL_BACKEND',
     'django.core.mail.backends.console.EmailBackend',
 )
+EMAIL_HOST = os.getenv('EMAIL_HOST', '')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'true').lower() in {'1', 'true', 'yes', 'on'}
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'false').lower() in {'1', 'true', 'yes', 'on'}
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'alerts@localhost')
 ALERT_EMAIL_TO = os.getenv('ALERT_EMAIL_TO', 'langlois88@gmail.com')
 ALERT_PRICE_THRESHOLD = float(os.getenv('ALERT_PRICE_THRESHOLD', '1000'))
