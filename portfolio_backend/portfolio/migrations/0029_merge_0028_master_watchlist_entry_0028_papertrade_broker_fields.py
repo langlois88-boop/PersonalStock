f3b38f24de057b172cf51213dff5d9819d699b8b
Eric Langlois <langlois88@gmail.com>
2026-02-26 18:41:05 -0500

    Merge 0028 migrations

---
+++ portfolio_backend/portfolio/migrations/0029_merge_0028_master_watchlist_entry_0028_papertrade_broker_fields.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("portfolio", "0028_master_watchlist_entry"),
+        ("portfolio", "0028_papertrade_broker_fields"),
+    ]
+
+    operations = []
