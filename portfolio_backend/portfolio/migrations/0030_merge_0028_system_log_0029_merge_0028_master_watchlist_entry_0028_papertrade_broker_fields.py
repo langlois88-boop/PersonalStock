c8964cac7354a5afca27bea9a23158cfc6c70bad
Eric Langlois <langlois88@gmail.com>
2026-02-26 20:55:21 -0500

    Merge system log migration

---
+++ portfolio_backend/portfolio/migrations/0030_merge_0028_system_log_0029_merge_0028_master_watchlist_entry_0028_papertrade_broker_fields.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portfolio', '0028_system_log'),
+        ('portfolio', '0029_merge_0028_master_watchlist_entry_0028_papertrade_broker_fields'),
+    ]
+
+    operations = []
